940603f92782542f0fb0452037aab96388daff4f
jaseel-koolath <112375897+jaseel-koolath@users.noreply.github.com>
2022-12-06 23:06:54 +0530

    Real Estate website first commit

---
+++ src/components/NewListings.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function NewListings() {
+  return (
+    <div className="max-w-[95vw] lg:max-w-[1280px] mx-auto">
+      <div>
+        <p>Newest listings</p>
+        <p>See the most up-to-date listings</p>
+      </div>
+    </div>
+  );
+}
+
+export default NewListings;
